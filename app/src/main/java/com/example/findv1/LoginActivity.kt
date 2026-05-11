9bfa604c74a813a6e6d2351dcdf4f99fecd5c0ab
RikaSpark320 <155036358+RikaSpark320@users.noreply.github.com>
2026-05-10 19:54:43 -0600

    ya se arregló lo del inicio de sesion, si crea la cuenta pero no deja inciar dice error

--- app/src/main/java/com/example/findv1/LoginActivity.kt
+++ app/src/main/java/com/example/findv1/LoginActivity.kt
@@ -36,7 +36,7 @@ class LoginActivity : AppCompatActivity() {
                             startActivity(Intent(this, MainActivity::class.java))
                             finish()
                         } else {
-                            Toast.makeText(this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "Error: ${task.exception?.message}", Toast.LENGTH_LONG).show()
                         }
                     }
             } else {
